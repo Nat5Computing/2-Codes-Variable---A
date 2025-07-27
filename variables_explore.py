e68fc61c5bbf269f3084c662eb3884d70d7a5058
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-28 00:27:27 +0100

    Create variables_explore.py

---
+++ variables_explore.py
@@ -0,0 +1,15 @@
+# Edit the values of the variables below
+
+favouriteApp = 'Instagram'
+luckyNumber = 9
+decimalNumber = 3.5
+firstInitial = 'F'
+vote = True
+
+# Print out all the information
+
+print('My favourite app is ' + favouriteApp)
+print('My favourite lucky number is ' + str(luckyNumber))
+print('My decimal number is ' + str(decimalNumber))
+print('My first initial of my first name is ' + firstInitial)
+print('I love pineapple on a pizza!  ' + str(vote))
